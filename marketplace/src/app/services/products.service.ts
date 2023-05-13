e3615fa2ba3fff850e1a87386324de5720194d2f
David Cartagena <mbinladen9@gmail.com>
2023-05-13 15:12:52 -0500

    Ajuste para dist de la aplicacion

--- marketplace/src/app/services/products.service.ts
+++ marketplace/src/app/services/products.service.ts
@@ -6,7 +6,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ProductsService {
-
   private urlApi = 'https://api.escuelajs.co/api/v1/products';
 
   constructor(private http: HttpClient) {
@@ -18,21 +17,17 @@ export class ProductsService {
   }
 
   // revisar
-  public buscarProduct(termino: string){
-    let buscarProductArr: product[] = [];
+  public buscarProduct(termino: string) {
+    //let buscarProductArr: product[] = [];
     // termino = termino.toLowerCase();
-
     // for (let i = 0; i < this.product.length; i++) {
     //   let heroe = this.product[i];
-
     //   let nombre = product.nombre.toLowerCase();
-
     //   if (nombre.indexOf(termino) >= 0) {
     //     heroe.idx = i;
     //     heroesArr.push(heroe);
     //   }
     // }
-
     // return heroesArr;
   }
 }
