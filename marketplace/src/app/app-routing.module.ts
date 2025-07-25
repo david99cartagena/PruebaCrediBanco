252041e0c46c718de13d5fc815501bd1da03b2d5
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-25 01:05:04 -0500

    Switch to hash routing and clean up Netlify redirects

--- marketplace/src/app/app-routing.module.ts
+++ marketplace/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { CarritoComponent } from './components/carrito/carrito.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'products',pathMatch:'full'},
-  {path:'products', component: ProductsComponent},
-  {path:'carrito', component: CarritoComponent}
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: '**', redirectTo: 'products' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 
